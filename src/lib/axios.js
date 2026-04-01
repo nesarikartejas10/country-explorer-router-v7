9948553dc149434f0b3241c3dc1617eaf0731875
nesarikartejas10 <nesarikartejas10@gmail.com>
2026-04-01 20:29:06 +0530

    create countries service to fetch data with centralized error handling

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_URL,
   withCredentials: true,
+  timeout: 5000,
 });
 
 export default api;
--- src/services/countryApi.js
+++ src/services/countryApi.js
@@ -1,1 +1,12 @@
-export const getAllCountries = async () => {};
+import toast from "react-hot-toast";
+import api from "../lib/axios";
+
+export const getAllCountries = async () => {
+  try {
+    const res = await api.get("/all?fields=name,flags");
+    return res.data;
+  } catch (error) {
+    toast.error("Failed to fetch countries");
+    return [];
+  }
+};
